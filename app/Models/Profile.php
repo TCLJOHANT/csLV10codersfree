7078cafc2e4488760fe5bdac1ba0502eba634e02
johanChte <jachate7qmisena.edu.co>
2023-08-29 09:41:02 -0500

    recuperar usuario de manera  manual)

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     use HasFactory;
+    //recuperar inforacion de usuario
+    public function user(){
+        $user = User::find($this->user_id);
+        return $user;
+    }
 }
